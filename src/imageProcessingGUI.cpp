6f651d0ba65110bcc3637cb6fc8fad5ad3d64609
Lucas Barreto <lucas.barreto@icomp.ufam.edu.br>
2024-10-14 19:28:00 -0400

    implementação de uma tela que contem botao que abre uma imagem, mostrando na tela a imagem e seu caminho

--- src/imageProcessingGUI.cpp
+++ src/imageProcessingGUI.cpp
@@ -1,72 +1,109 @@
-// main.cpp
 #include <wx/wx.h>
-#include <wx/sizer.h>  // Necessário para gerenciar o layout com sizers
+#include <wx/image.h>
+#include <wx/dcclient.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+class MyFrame : public wxFrame {
+public:
+    MyFrame();
+
+private:
+    void OnOpen(wxCommandEvent& event);
+    void OnPaint(wxPaintEvent& event);
+    wxBitmap LoadImage(const wxString& filename);
+
+    wxBitmap m_bitmap;              // Bitmap para armazenar a imagem carregada
+    wxStaticText* m_filePathLabel;  // Label para exibir o caminho do arquivo
+
+    wxDECLARE_EVENT_TABLE();
+};
+
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+    EVT_MENU(wxID_OPEN, MyFrame::OnOpen)
+    EVT_PAINT(MyFrame::OnPaint)
+wxEND_EVENT_TABLE()
 
 class MyApp : public wxApp {
 public:
     virtual bool OnInit();
 };
 
-class MyFrame : public wxFrame {
-public:
-    MyFrame(const wxString& title);
-
-private:
-    void OnExit(wxCommandEvent& event);
-    wxDECLARE_EVENT_TABLE();
-};
-
-wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-    EVT_MENU(wxID_EXIT, MyFrame::OnExit)
-wxEND_EVENT_TABLE()
-
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit() {
-    MyFrame* frame = new MyFrame("Image Processing App");
+    MyFrame* frame = new MyFrame();
     frame->Show(true);
     return true;
 }
 
-MyFrame::MyFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title) {
-    SetSize(800, 600);
-    CreateStatusBar();
-    SetStatusText("Welcome to Image Processing App!");
-
-    // Menu
-    wxMenu* menuFile = new wxMenu;
-    menuFile->Append(wxID_EXIT, "&Exit\tCtrl-Q", "Quit this program");
-    wxMenuBar* menuBar = new wxMenuBar;
+MyFrame::MyFrame()
+    : wxFrame(nullptr, wxID_ANY, "Impact Lab: Image Processing GUI", wxDefaultPosition, wxSize(1200, 800)) {
+    wxMenuBar* menuBar = new wxMenuBar();
+    wxMenu* menuFile = new wxMenu();
+    menuFile->Append(wxID_OPEN, "&Open\tCtrl-O", "Open an image file");
     menuBar->Append(menuFile, "&File");
     SetMenuBar(menuBar);
 
-    // Layout
-    wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+    // Painel para organizar o layout
+    wxPanel* panel = new wxPanel(this);
+    wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
 
-    // Placeholder para a área da imagem
-    wxStaticText* imageArea = new wxStaticText(this, wxID_ANY, "Image Display Area",
-                                               wxDefaultPosition, wxSize(600, 400), wxALIGN_CENTER);
-    mainSizer->Add(imageArea, 1, wxEXPAND | wxALL, 10);
+    // Adicionando um botão "Abrir Imagem"
+    wxButton* openButton = new wxButton(panel, wxID_ANY, "Abrir Imagem", wxDefaultPosition, wxSize(120, 30));
+    openButton->Bind(wxEVT_BUTTON, &MyFrame::OnOpen, this);
+    
+    sizer->Add(openButton, 0, wxALIGN_CENTER | wxTOP | wxBOTTOM, 10);
+    
+    // Adicionando um label para mostrar o caminho do arquivo
+    m_filePathLabel = new wxStaticText(panel, wxID_ANY, "Caminho do arquivo: ", wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER);
+    sizer->Add(m_filePathLabel, 0, wxALIGN_CENTER | wxBOTTOM, 10);
 
-    // Placeholder para os botões de filtro
-    wxBoxSizer* buttonSizer = new wxBoxSizer(wxHORIZONTAL);
-    wxButton* filterButton1 = new wxButton(this, wxID_ANY, "Filter 1");
-    wxButton* filterButton2 = new wxButton(this, wxID_ANY, "Filter 2");
-    wxButton* filterButton3 = new wxButton(this, wxID_ANY, "Filter 3");
-    buttonSizer->Add(filterButton1, 1, wxEXPAND | wxALL, 5);
-    buttonSizer->Add(filterButton2, 1, wxEXPAND | wxALL, 5);
-    buttonSizer->Add(filterButton3, 1, wxEXPAND | wxALL, 5);
-
-    mainSizer->Add(buttonSizer, 0, wxALIGN_CENTER | wxBOTTOM, 10);
-
-    // Placeholder para o tempo de execução
-    wxStaticText* execTimeLabel = new wxStaticText(this, wxID_ANY, "Execution Time: N/A",
-                                                   wxDefaultPosition, wxSize(200, 30), wxALIGN_CENTER);
-    mainSizer->Add(execTimeLabel, 0, wxALIGN_CENTER | wxALL, 10);
-
-    SetSizer(mainSizer);
+    panel->SetSizer(sizer);
 }
 
-void MyFrame::OnExit(wxCommandEvent& event) {
-    Close(true);
+wxBitmap MyFrame::LoadImage(const wxString& filename) {
+    int width, height, channels;
+    unsigned char* data = stbi_load(filename.ToStdString().c_str(), &width, &height, &channels, 0);
+
+    if (data) {
+        wxImage img(width, height, data, true);
+        stbi_image_free(data);
+        return wxBitmap(img);  // Converte wxImage para wxBitmap
+    } else {
+        wxLogError("Failed to load image: %s", filename);
+        return wxBitmap();
+    }
+}
+
+void MyFrame::OnOpen(wxCommandEvent& event) {
+    wxFileDialog openFileDialog(this, "Open image file", "", "", 
+        "Image files (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png", 
+        wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+
+    if (openFileDialog.ShowModal() == wxID_OK) {
+        wxString path = openFileDialog.GetPath();
+        m_bitmap = LoadImage(path);  // Carrega a imagem e a armazena em m_bitmap
+        
+        // Atualiza o label com o caminho do arquivo
+        m_filePathLabel->SetLabel("Caminho do arquivo: " + path);
+        
+        Refresh();  // Redesenha a janela para exibir a nova imagem
+    }
+}
+
+void MyFrame::OnPaint(wxPaintEvent& event) {
+    wxPaintDC dc(this);
+    if (m_bitmap.IsOk()) {
+        // Obtém as dimensões da janela e da imagem
+        int windowWidth, windowHeight;
+        GetClientSize(&windowWidth, &windowHeight);
+        int imgWidth = m_bitmap.GetWidth();
+        int imgHeight = m_bitmap.GetHeight();
+
+        int x = (windowWidth - imgWidth) / 2;  
+        int y = 50 + 30 + 10;  
+
+        dc.DrawBitmap(m_bitmap, x, y, false);  
+    }
 }
